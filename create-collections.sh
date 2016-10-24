56b01ed18457f2fc45cfa8e731f199be18a4edcc
PiDiaries <iainjohnston79@gmail.com>
2016-10-24 10:01:43 +0100

    Add files via upload
    Script to create RetroFE collections and symlink to RetroPie Rom folders.

---
+++ create-collections.sh
@@ -0,0 +1,286 @@
+#!/usr/bin/env bash
+
+cd ~/RetroFE
+
+./retrofe -createcollection "3do"
+rmdir ~/RetroFE/collections/3do/roms
+ln -s ~/RetroPie/roms/3do ~/RetroFE/collections/3do/roms
+
+./retrofe -createcollection "ags"
+rmdir ~/RetroFE/collections/ags/roms
+ln -s ~/RetroPie/roms/ags ~/RetroFE/collections/ags/roms
+
+./retrofe -createcollection "amiga"
+rmdir ~/RetroFE/collections/amiga/roms
+ln -s ~/RetroPie/roms/amiga ~/RetroFE/collections/amiga/roms
+
+./retrofe -createcollection "amstradcpc"
+rmdir ~/RetroFE/collections/amstradcpc/roms
+ln -s ~/RetroPie/roms/amstradcpc ~/RetroFE/collections/amstradcpc/roms
+
+./retrofe -createcollection "apple"
+rmdir ~/RetroFE/collections/apple/roms
+ln -s ~/RetroPie/roms/apple ~/RetroFE/collections/apple/roms
+
+rmdir ~/RetroFE/collections/Arcade/roms
+ln -s ~/RetroPie/roms/arcade ~/RetroFE/collections/Arcade/roms
+
+./retrofe -createcollection "arcadia"
+rmdir ~/RetroFE/collections/arcadia/roms
+ln -s ~/RetroPie/roms/arcadia ~/RetroFE/collections/arcadia/roms
+
+./retrofe -createcollection "atari800"
+rmdir ~/RetroFE/collections/atari800/roms
+ln -s ~/RetroPie/roms/atari800 ~/RetroFE/collections/atari800/roms
+
+./retrofe -createcollection "atari2600"
+rmdir ~/RetroFE/collections/atari2600/roms
+ln -s ~/RetroPie/roms/atari2600 ~/RetroFE/collections/atari2600/roms
+
+./retrofe -createcollection "atari5200"
+rmdir ~/RetroFE/collections/atari5200/roms
+ln -s ~/RetroPie/roms/atari5200 ~/RetroFE/collections/atari5200/roms
+
+./retrofe -createcollection "atari7800"
+rmdir ~/RetroFE/collections/atari7800/roms
+ln -s ~/RetroPie/roms/atari7800 ~/RetroFE/collections/atari7800/roms
+
+./retrofe -createcollection "atarijaguar"
+rmdir ~/RetroFE/collections/3do/roms
+ln -s ~/RetroPie/roms/3do ~/RetroFE/collections/3do/roms
+
+./retrofe -createcollection "atarilynx"
+rmdir ~/RetroFE/collections/atarilynx/roms
+ln -s ~/RetroPie/roms/atarilynx ~/RetroFE/collections/atarilynx/roms
+
+./retrofe -createcollection "atarist"
+rmdir ~/RetroFE/collections/atarist/roms
+ln -s ~/RetroPie/roms/atarist ~/RetroFE/collections/atarist/roms
+
+./retrofe -createcollection "c64"
+rmdir ~/RetroFE/collections/c64/roms
+ln -s ~/RetroPie/roms/c64 ~/RetroFE/collections/c64/roms
+
+./retrofe -createcollection "coco"
+rmdir ~/RetroFE/collections/coco/roms
+ln -s ~/RetroPie/roms/coco ~/RetroFE/collections/coco/roms
+
+./retrofe -createcollection "coleco"
+rmdir ~/RetroFE/collections/coleco/roms
+ln -s ~/RetroPie/roms/coleco ~/RetroFE/collections/coleco/roms
+
+./retrofe -createcollection "crvision"
+rmdir ~/RetroFE/collections/crvision/roms
+ln -s ~/RetroPie/roms/crvision ~/RetroFE/collections/crvision/roms
+
+./retrofe -createcollection "dragon32"
+rmdir ~/RetroFE/collections/dragon32/roms
+ln -s ~/RetroPie/roms/dragon32 ~/RetroFE/collections/dragon32/roms
+
+./retrofe -createcollection "dreamcast"
+rmdir ~/RetroFE/collections/dreamcast/roms
+ln -s ~/RetroPie/roms/dreamcast ~/RetroFE/collections/dreamcast/roms
+
+./retrofe -createcollection "fba"
+rmdir ~/RetroFE/collections/fba/roms
+ln -s ~/RetroPie/roms/fba ~/RetroFE/collections/fba/roms
+
+./retrofe -createcollection "fds"
+rmdir ~/RetroFE/collections/fds/roms
+ln -s ~/RetroPie/roms/fds ~/RetroFE/collections/fds/roms
+
+./retrofe -createcollection "gameandwatch"
+rmdir ~/RetroFE/collections/gameandwatch/roms
+ln -s ~/RetroPie/roms/gameandwatch ~/RetroFE/collections/gameandwatch/roms
+
+./retrofe -createcollection "gameboy"
+rmdir ~/RetroFE/collections/gameboy/roms
+ln -s ~/RetroPie/roms/gameboy ~/RetroFE/collections/gameboy/roms
+
+./retrofe -createcollection "gamegear"
+rmdir ~/RetroFE/collections/gamegear/roms
+ln -s ~/RetroPie/roms/gamegear ~/RetroFE/collections/gamegear/roms
+
+./retrofe -createcollection "gb"
+rmdir ~/RetroFE/collections/gb/roms
+ln -s ~/RetroPie/roms/gb ~/RetroFE/collections/gb/roms
+
+./retrofe -createcollection "gba"
+rmdir ~/RetroFE/collections/gba/roms
+ln -s ~/RetroPie/roms/gba ~/RetroFE/collections/gba/roms
+
+./retrofe -createcollection "gbc"
+rmdir ~/RetroFE/collections/gbc/roms
+ln -s ~/RetroPie/roms/gbc ~/RetroFE/collections/gbc/roms
+
+./retrofe -createcollection "gc"
+rmdir ~/RetroFE/collections/gc/roms
+ln -s ~/RetroPie/roms/gc ~/RetroFE/collections/gc/roms
+
+./retrofe -createcollection "intellivision"
+rmdir ~/RetroFE/collections/intellivision/roms
+ln -s ~/RetroPie/roms/intellivision ~/RetroFE/collections/intellivision/roms
+
+./retrofe -createcollection "love"
+rmdir ~/RetroFE/collections/love/roms
+ln -s ~/RetroPie/roms/love ~/RetroFE/collections/love/roms
+
+./retrofe -createcollection "macintosh"
+rmdir ~/RetroFE/collections/macintosh/roms
+ln -s ~/RetroPie/roms/macintosh ~/RetroFE/collections/macintosh/roms
+
+./retrofe -createcollection "mame-advmame"
+rmdir ~/RetroFE/collections/mame-advmame/roms
+ln -s ~/RetroPie/roms/mame-advmame ~/RetroFE/collections/mame-advmame/roms
+
+./retrofe -createcollection "mame-libretro"
+rmdir ~/RetroFE/collections/mame-libretro/roms
+ln -s ~/RetroPie/roms/mame-libretro ~/RetroFE/collections/mame-libretro/roms
+
+./retrofe -createcollection "mame-mame4all"
+rmdir ~/RetroFE/collections/mame-mame4all/roms
+ln -s ~/RetroPie/roms/mame-mame4all ~/RetroFE/collections/mame-mame4all/roms
+
+./retrofe -createcollection "mastersystem"
+rmdir ~/RetroFE/collections/mastersystem/roms
+ln -s ~/RetroPie/roms/mastersystem ~/RetroFE/collections/mastersystem/roms
+
+./retrofe -createcollection "megadrive"
+rmdir ~/RetroFE/collections/megadrive/roms
+ln -s ~/RetroPie/roms/megadrive ~/RetroFE/collections/megadrive/roms
+
+./retrofe -createcollection "msx"
+rmdir ~/RetroFE/collections/msx/roms
+ln -s ~/RetroPie/roms/msx ~/RetroFE/collections/msx/roms
+
+./retrofe -createcollection "n64"
+rmdir ~/RetroFE/collections/n64/roms
+ln -s ~/RetroPie/roms/n64 ~/RetroFE/collections/n64/roms
+
+./retrofe -createcollection "nds"
+rmdir ~/RetroFE/collections/nds/roms
+ln -s ~/RetroPie/roms/nds ~/RetroFE/collections/nds/roms
+
+./retrofe -createcollection "neogeo"
+rmdir ~/RetroFE/collections/neogeo/roms
+ln -s ~/RetroPie/roms/neogeo ~/RetroFE/collections/neogeo/roms
+
+./retrofe -createcollection "nes"
+rmdir ~/RetroFE/collections/nes/roms
+ln -s ~/RetroPie/roms/nes ~/RetroFE/collections/nes/roms
+
+./retrofe -createcollection "ngp"
+rmdir ~/RetroFE/collections/ngp/roms
+ln -s ~/RetroPie/roms/ngp ~/RetroFE/collections/ngp/roms
+
+./retrofe -createcollection "ngpc"
+rmdir ~/RetroFE/collections/ngpc/roms
+ln -s ~/RetroPie/roms/ngpc ~/RetroFE/collections/ngpc/roms
+
+./retrofe -createcollection "oric"
+rmdir ~/RetroFE/collections/oric/roms
+ln -s ~/RetroPie/roms/oric ~/RetroFE/collections/oric/roms
+
+./retrofe -createcollection "pc"
+rmdir ~/RetroFE/collections/pc/roms
+ln -s ~/RetroPie/roms/pc ~/RetroFE/collections/pc/roms
+
+./retrofe -createcollection "pcengine"
+rmdir ~/RetroFE/collections/pcengine/roms
+ln -s ~/RetroPie/roms/pcengine ~/RetroFE/collections/pcengine/roms
+
+./retrofe -createcollection "pcfx"
+rmdir ~/RetroFE/collections/pcfx/roms
+ln -s ~/RetroPie/roms/pcfx ~/RetroFE/collections/pcfx/roms
+
+./retrofe -createcollection "ports"
+rmdir ~/RetroFE/collections/ports/roms
+ln -s ~/RetroPie/roms/ports ~/RetroFE/collections/ports/roms
+
+./retrofe -createcollection "psp"
+rmdir ~/RetroFE/collections/psp/roms
+ln -s ~/RetroPie/roms/psp ~/RetroFE/collections/psp/roms
+
+./retrofe -createcollection "psx"
+rmdir ~/RetroFE/collections/psx/roms
+ln -s ~/RetroPie/roms/psx ~/RetroFE/collections/psx/roms
+
+./retrofe -createcollection "residualvm"
+rmdir ~/RetroFE/collections/residualvm/roms
+ln -s ~/RetroPie/roms/residualvm ~/RetroFE/collections/residualvm/roms
+
+./retrofe -createcollection "samcoupe"
+rmdir ~/RetroFE/collections/samcoupe/roms
+ln -s ~/RetroPie/roms/samcoupe ~/RetroFE/collections/samcoupe/roms
+
+./retrofe -createcollection "saturn"
+rmdir ~/RetroFE/collections/saturn/roms
+ln -s ~/RetroPie/roms/saturn ~/RetroFE/collections/saturn/roms
+
+./retrofe -createcollection "scummvm"
+rmdir ~/RetroFE/collections/scummvm/roms
+ln -s ~/RetroPie/roms/scummvm ~/RetroFE/collections/scummvm/roms
+
+./retrofe -createcollection "sega32x"
+rmdir ~/RetroFE/collections/sega32x/roms
+ln -s ~/RetroPie/roms/sega32x ~/RetroFE/collections/sega32x/roms
+
+./retrofe -createcollection "segacd"
+rmdir ~/RetroFE/collections/segacd/roms
+ln -s ~/RetroPie/roms/segacd ~/RetroFE/collections/segacd/roms
+
+./retrofe -createcollection "sg-1000"
+rmdir ~/RetroFE/collections/sg-1000/roms
+ln -s ~/RetroPie/roms/sg-1000 ~/RetroFE/collections/sg-1000/roms
+
+./retrofe -createcollection "snes"
+rmdir ~/RetroFE/collections/snes/roms
+ln -s ~/RetroPie/roms/snes ~/RetroFE/collections/snes/roms
+
+./retrofe -createcollection "stratagus"
+rmdir ~/RetroFE/collections/stratagus/roms
+ln -s ~/RetroPie/roms/stratagus ~/RetroFE/collections/stratagus/roms
+
+./retrofe -createcollection "ti99"
+rmdir ~/RetroFE/collections/ti99/roms
+ln -s ~/RetroPie/roms/ti99 ~/RetroFE/collections/ti99/roms
+
+./retrofe -createcollection "trs-80"
+rmdir ~/RetroFE/collections/trs-80/roms
+ln -s ~/RetroPie/roms/trs-80 ~/RetroFE/collections/trs-80/roms
+
+./retrofe -createcollection "vectrex"
+rmdir ~/RetroFE/collections/vectrex/roms
+ln -s ~/RetroPie/roms/vectrex ~/RetroFE/collections/vectrex/roms
+
+./retrofe -createcollection "videopac"
+rmdir ~/RetroFE/collections/videopac/roms
+ln -s ~/RetroPie/roms/videopac ~/RetroFE/collections/videopac/roms
+
+./retrofe -createcollection "virtualboy"
+rmdir ~/RetroFE/collections/virtualboy/roms
+ln -s ~/RetroPie/roms/virtualboy ~/RetroFE/collections/virtualboy/roms
+
+./retrofe -createcollection "wonderswan"
+rmdir ~/RetroFE/collections/wonderswan/roms
+ln -s ~/RetroPie/roms/wonderswan ~/RetroFE/collections/wonderswan/roms
+
+./retrofe -createcollection "wonderswancolor"
+rmdir ~/RetroFE/collections/wonderswancolor/roms
+ln -s ~/RetroPie/roms/wonderswancolor ~/RetroFE/collections/wonderswancolor/roms
+./retrofe -createcollection "x68000"
+rmdir ~/RetroFE/collections/x68000/roms
+ln -s ~/RetroPie/roms/x68000 ~/RetroFE/collections/x68000/roms
+
+./retrofe -createcollection "zmachine"
+rmdir ~/RetroFE/collections/zmachine/roms
+ln -s ~/RetroPie/roms/zmachine ~/RetroFE/collections/zmachine/roms
+
+./retrofe -createcollection "zxspectrum"
+rmdir ~/RetroFE/collections/zxspectrum/roms
+ln -s ~/RetroPie/roms/zxspectrum ~/RetroFE/collections/zxspectrum/roms
+
+rmdir ~/RetroFE/collections/Sega\ Genesis/roms
+ln -s ~/RetroPie/roms/megadrive ~/RetroFE/collections/Sega\ Genesis/roms
+
+end
